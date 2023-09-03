b8c5e2a7d80f50e2d0912744c74055209b716727
DanaSF7 <137504086+DanaSF7@users.noreply.github.com>
2023-09-03 21:23:40 +0400

    Initial Commit

---
+++ home/homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeApp.swift
+//  home
+//
+//  Created by Dana Al Fanek on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
