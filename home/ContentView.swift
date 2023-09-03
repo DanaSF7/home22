8053059fba725beb9fbeb2a633882d83839e0553
DanaSF7 <137504086+DanaSF7@users.noreply.github.com>
2023-09-03 21:53:19 +0400

    yaa

--- home/ContentView.swift
+++ home/ContentView.swift
@@ -10,17 +10,78 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            NavigationView {
+                ScrollView(.vertical) {
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20.0 )
+                            .frame (width: 360, height: 110) .foregroundColor (Color.red)
+                        
+                        Text("Gratitude")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20.0 )
+                            .frame (width: 360, height: 110) .foregroundColor (Color.orange)
+                        Text("Caring")
+                            .foregroundColor(.white)
+                            .font(.title)
+                        
+                    }
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20.0 )
+                            .frame (width: 360, height: 110) .foregroundColor (Color.yellow)
+                        Text("Self confidence")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20.0 )
+                            .frame (width: 360, height: 110) .foregroundColor (Color.green)
+                        Text("Affirmations")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20.0 )
+                            .frame (width: 360, height: 110) .foregroundColor (Color.blue)
+                        Text("healthy relationships")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20.0 )
+                            .frame (width: 360, height: 110) .foregroundColor (Color.purple)
+                        Text("self tolerance")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20.0 )
+                            .frame (width: 360, height: 110) .foregroundColor (Color.pink)
+                        Text("self appretiation")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    
+                }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            
+                .navigationTitle("Daily Check in!")
+                
+            }
+            .padding()
+        }
     }
 }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
